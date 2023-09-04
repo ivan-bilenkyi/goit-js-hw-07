36848f515fbfae7553079966c786e41433843294
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2023-09-04 21:28:42 +0300

    fix problems

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,16 +22,14 @@ gallery.insertAdjacentHTML("beforeend", markup);
 
 gallery.addEventListener("click", onClickShow);
 function onClickShow(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
   const { target } = event;
   event.preventDefault();
+
   const instance = basicLightbox.create(`
       <img class="gallery__image" src="${target.dataset.source}" alt="${target.alt}" width="800" height="600">
   `);
   instance.show();
-
-  document.addEventListener("keydown", (event) => {
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  });
 }
